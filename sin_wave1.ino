41a21f5177e2caa3d61ed5285e93e8b71018ad13
chibaf <chibaf@mac.com>
2023-12-02 12:04:00 +0900

    Add files via upload

---
+++ sin_wave1.ino
@@ -0,0 +1,18 @@
+void setup()
+{
+  // initialize serial comms
+  Serial.begin(9600); 
+}
+
+int i=0;
+float x;
+float pi=3.1415927; 
+void loop()
+{
+  if (i==99) i=0;
+  x=pi*float(i)/50.0;
+  float sin1 = sin(x);
+  i=i+1;
+  delay(100);
+  Serial.println(sin1);
+}
